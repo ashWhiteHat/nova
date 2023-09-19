2267cb62c2260ce8756eeb906be2f7d7be39348d
ashWhiteHat <phantomofrotten@gmail.com>
2023-09-19 12:48:33 +0900

    test r1cs with example

--- src/r1cs.rs
+++ src/r1cs.rs
@@ -34,18 +34,18 @@ impl<F: PrimeField> R1cs<F> {
 pub(crate) struct SparseMatrix<F: PrimeField>(Vec<Vec<Element<F>>>);
 
 impl<F: PrimeField> SparseMatrix<F> {
-    fn new(value: Vec<Vec<F>>) -> Self {
-        let mut sparse_matrix = vec![];
-        for elements in value {
-            let mut rows = vec![];
+    pub(crate) fn product(&self, witnesses: &Vec<F>) -> F {
+        self.0.iter().fold(F::zero(), |mut sum, elements| {
             for element in elements {
-                if !element.is_zero() {
-                    rows.push(element.into())
-                }
+                let (wire, value) = (element.0, element.1);
+                let index = match wire {
+                    Wire::Instance(index) => index,
+                    Wire::Witness(index) => index,
+                };
+                sum += witnesses[index] * value;
             }
-            sparse_matrix.push(rows)
-        }
-        Self(sparse_matrix)
+            sum
+        })
     }
 }
 
@@ -61,5 +61,69 @@ impl<F: PrimeField> From<Wire> for Element<F> {
 impl<F: PrimeField> From<F> for Element<F> {
     fn from(value: F) -> Self {
         Self(Wire::one(), value)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Element, PrimeField, R1cs, SparseMatrix, Wire};
+
+    use bls_12_381::Fr as Scalar;
+
+    fn index_value_to_element<F: PrimeField>(wire: Wire, value: F) -> Element<F> {
+        Element(wire, value)
+    }
+
+    fn dense_to_sparse<F: PrimeField>(value: Vec<Vec<u64>>) -> SparseMatrix<F> {
+        let mut sparse_matrix = vec![];
+        for elements in value {
+            let mut rows = vec![];
+            for (index, element) in elements.iter().enumerate() {
+                if !(*element == 0) {
+                    let instance = Wire::instance(index);
+                    let value = F::from(*element).into();
+                    rows.push(index_value_to_element(instance, value))
+                }
+            }
+            sparse_matrix.push(rows)
+        }
+        SparseMatrix(sparse_matrix)
+    }
+
+    fn is_satisfy<F: PrimeField>(r1cs: R1cs<F>, witnesses: Vec<u64>) -> bool {
+        let witnesses = witnesses.iter().map(|witness| F::from(*witness)).collect();
+        let a = r1cs.a.product(&witnesses);
+        let b = r1cs.b.product(&witnesses);
+        let c = r1cs.c.product(&witnesses);
+        println!("a: {:?} b: {:?} c: {:?}", a, b, c);
+        a * b == c
+    }
+
+    #[test]
+    fn r1cs_test() {
+        // R1CS for: x^3 + x + 5 = y
+        // https://www.vitalik.ca/general/2016/12/10/qap.html
+        let m = 3;
+        let a = dense_to_sparse::<Scalar>(vec![
+            vec![0, 1, 0, 0, 0, 0],
+            // vec![0, 0, 0, 1, 0, 0],
+            // vec![0, 1, 0, 0, 1, 0],
+            // vec![5, 0, 0, 0, 0, 1],
+        ]);
+        let b = dense_to_sparse::<Scalar>(vec![
+            vec![0, 1, 0, 0, 0, 0],
+            // vec![0, 1, 0, 0, 0, 0],
+            // vec![1, 0, 0, 0, 0, 0],
+            // vec![1, 0, 0, 0, 0, 0],
+        ]);
+        let c = dense_to_sparse::<Scalar>(vec![
+            vec![0, 0, 0, 1, 0, 0],
+            // vec![0, 0, 0, 0, 1, 0],
+            // vec![0, 0, 0, 0, 0, 1],
+            // vec![0, 0, 1, 0, 0, 0],
+        ]);
+        let r1cs = R1cs { m, a, b, c };
+        let z = vec![1, 3, 35, 9, 27, 30];
+        assert!(is_satisfy(r1cs, z))
     }
 }
