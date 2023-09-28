2bdabd6b38427e296dc961701a8ddc3dee5d11b6
ashWhiteHat <phantomofrotten@gmail.com>
2023-09-28 21:29:19 +0900

    implement transcript logic

--- src/transcript.rs
+++ src/transcript.rs
@@ -1,6 +1,29 @@
-use zkstd::common::CurveAffine;
+use merlin::Transcript;
+use zkstd::common::{CurveAffine, FftField};
 
-pub trait Transcript<C: CurveAffine> {
-    fn absorb_point(&mut self, v: &C);
-    fn get_challenge(&mut self) -> C::Scalar;
+pub trait ChallengeTranscript<C: CurveAffine> {
+    fn append_point(&mut self, label: &'static [u8], v: &C);
+
+    fn append_scalar(&mut self, label: &'static [u8], v: &C::Scalar);
+
+    fn challenge_scalar(&mut self, label: &'static [u8]) -> C::Scalar;
+}
+
+impl<C: CurveAffine> ChallengeTranscript<C> for Transcript {
+    fn append_point(&mut self, label: &'static [u8], v: &C) {
+        let x = C::Scalar::from(v.get_x());
+        let y = C::Scalar::from(v.get_y());
+        <Transcript as ChallengeTranscript<C>>::append_scalar(self, label, &x);
+        <Transcript as ChallengeTranscript<C>>::append_scalar(self, label, &y);
+    }
+
+    fn append_scalar(&mut self, label: &'static [u8], v: &C::Scalar) {
+        self.append_message(label, &v.to_raw_bytes())
+    }
+
+    fn challenge_scalar(&mut self, label: &'static [u8]) -> C::Scalar {
+        let mut buf = [0u8; 64];
+        self.challenge_bytes(label, &mut buf);
+        C::Scalar::from_bytes_wide(&buf)
+    }
 }
